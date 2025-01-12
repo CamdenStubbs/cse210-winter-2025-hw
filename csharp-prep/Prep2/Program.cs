ade37d5bf69e942ae28ee5c44d0e09e0043f02fa
CamdenStubbs <camdenstubbs@byui.edu>
2025-01-11 21:14:22 -0700

    Prep 1 & 2 Completed

--- csharp-prep/Prep2/Program.cs
+++ csharp-prep/Prep2/Program.cs
@@ -4,6 +4,46 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep2 World!");
+        string letter = "";
+        Console.WriteLine("What is your current grade percentage? Please input an integer between 0-100. ");
+        string grade = Console.ReadLine();
+        int gradeNumber = int.Parse(grade);
+
+        if (gradeNumber >= 90)
+            {
+                letter = "A";
+            }
+        
+        else if (gradeNumber >= 80 && gradeNumber < 90)
+            {
+                letter = "B";
+            }
+
+        else if (gradeNumber >= 70 && gradeNumber < 80)
+            {
+                letter = "C";
+            }
+        
+        else if (gradeNumber >= 60 && gradeNumber < 70)
+            {
+                letter = "D";
+            }
+        
+        else if (gradeNumber < 60)
+            {
+                letter = "F";
+            }
+
+        Console.WriteLine($"You got a {letter}.");
+
+        if (gradeNumber >= 70)
+            { 
+                Console.WriteLine("Good Job! You passed!");
+            }
+        
+        else
+            {
+                Console.WriteLine("Better luck next time!");
+            }
     }
 }

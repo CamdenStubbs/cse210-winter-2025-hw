152a194a06a1581011d95631aa342f1661b942ec
CamdenStubbs <camdenstubbs@byui.edu>
2025-01-16 18:55:12 -0700

    Prep 3 and Prep 4 Completed

--- csharp-prep/Prep3/Program.cs
+++ csharp-prep/Prep3/Program.cs
@@ -4,6 +4,29 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep3 World!");
+        int guess = 0;
+        Random randomGenerator = new Random();
+        int magicNumber = randomGenerator.Next(1, 101);
+
+        while (magicNumber != guess)
+        {
+            Console.WriteLine("What is your guess? ");
+            guess = int.Parse(Console.ReadLine());
+
+            if (magicNumber > guess)
+                {
+                    Console.WriteLine("Higher");
+                }
+
+            else if (magicNumber < guess)
+                {
+                    Console.WriteLine("Lower");
+                }
+
+            else if (magicNumber == guess)
+                {
+                    Console.WriteLine("You Guessed it!");
+                }
+        }
     }
 }
--- csharp-prep/Prep4/Program.cs
+++ csharp-prep/Prep4/Program.cs
@@ -1,9 +1,37 @@
 using System;
+using System.Diagnostics.CodeAnalysis;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep4 World!");
+        int number;
+        int sum = 0;
+        int greatest = 0;
+        List<int> numbers = new List<int>();
+        Console.WriteLine("Please enter a list of positive and/or negative integers.\nType 0 when finished");
+        do
+        {
+            number = int.Parse(Console.ReadLine());
+            if (number > 0 || number < 0)
+            {
+                numbers.Add(number);
+            }
+        } while (number != 0);
+
+        foreach (int i in numbers)
+        {
+            sum += i;
+
+            if (i > greatest)
+            {
+                greatest = i;
+            }
+        }
+
+        float average = ((float)sum) / numbers.Count;
+        Console.WriteLine($"Sum of Numbers: {sum}");
+        Console.WriteLine($"Average of Numbers: {average}");
+        Console.WriteLine($"Highest Number: {greatest}");
     }
 }

be48acfc45b040e74c3d8ff9937d394922a9ee21
CamdenStubbs <camdenstubbs@byui.edu>
2025-02-24 04:33:45 -0700

    Prep Learning04

--- prepare/Learning04/Program.cs
+++ prepare/Learning04/Program.cs
@@ -4,6 +4,12 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Learning04 World!");
+        MathAssignment mathAssignment1 = new MathAssignment("Robbie Roddie", "Fractions", "Section 2.3", "Problems 1-3");
+        Console.WriteLine(mathAssignment1.GetSummary());
+        Console.WriteLine(mathAssignment1.GetHomeworkList());
+
+        WritingAssignment writingAssignment1 = new WritingAssignment("Lisa Roddie", "American History", "That One Essay");
+        Console.WriteLine(writingAssignment1.GetSummary());
+        Console.WriteLine(writingAssignment1.GetWritingInformation());  
     }
 }

75271cffb6088029966463e72ec7d05f34791dd6
CamdenStubbs <camdenstubbs@byui.edu>
2025-03-08 18:27:53 -0700

    Learning05

--- prepare/Learning05/Program.cs
+++ prepare/Learning05/Program.cs
@@ -4,6 +4,17 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Learning05 World!");
+        List<Shape> list = new List<Shape>();
+        Circle circle = new Circle("Orange", 14);
+        Square square = new Square("Purple", 14);
+        Rectangle rectangle = new Rectangle("Green", 10, 9);
+        list.Add(circle);
+        list.Add(square);
+        list.Add(rectangle);
+        foreach (Shape shape in list)
+        {
+            Console.WriteLine(shape.GetColor());
+            Console.WriteLine(shape.GetArea());
+        }
     }
 }
